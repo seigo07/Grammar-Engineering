dc985e8149b3e5dd509a147d9bc1e35b7f0314fb
seigo07 <seven_8s@icloud.com>
2023-03-13 22:02:34 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project
+
+Grammar engineering.
+
+## Description
+
+Grammar engineering.
+
+## Getting Started
+
+### Dependencies
+
+* Python Version: 3.9.13
+
+### Executing program
+
+* Please run the following command
+```
+```
---
+++ parse.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk import FeatureChartParser
+
+fcfg = nltk.data.load('grammar.fcfg')
+print(fcfg)
+parser = FeatureChartParser(fcfg)
+
+def parse_text(text):
+	examples = text.splitlines()
+	for sent in examples:
+		print(sent)
+		parses = parser.parse(sent.split())
+		for tree in parses:
+			print(tree)
+
+def parse_file(name):
+	f = open(name, 'r')
+	text = f.read()
+	f.close()
+	parse_text(text)
+
+print("================ Positive examples ================")
+parse_file('positives.txt')
+print("================ Negative examples ================")
+parse_file('negatives.txt')
